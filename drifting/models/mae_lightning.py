22e0a47a14a059141e8bc0889263bdde6cb95d63
Shen Tan <shentan@stu.hit.edu.cn>
2026-03-03 10:58:42 +0800

    Add: Class finetuning for feature encoder

--- drifting/models/mae_lightning.py
+++ drifting/models/mae_lightning.py
@@ -2,6 +2,8 @@ import lightning as L
 import torch
 from drifting.models.feature_encoder import MultiScaleFeatureEncoder, MAEEncoder
 from drifting.utils.vae_utils import VAEManager
+from torch import nn
+import torch.nn.functional as F
 
 #######################################################
 #                MAE LightningModule                  #
@@ -37,6 +39,15 @@ class MAEPretrainModule(L.LightningModule):
             mask_ratio=self.mae_config.get("mask_ratio", 0.75),
         )
 
+        ########### Optional classifier fine-tune (Appendix A.6) ###########
+        self.finetune_classifier = self.mae_config.get("finetune_classifier", False)
+        if self.finetune_classifier:
+            num_classes = config.get("num_classes", 10)
+            feat_dim = self.mae_config.get("feature_dim", 512)
+            self.cls_head = nn.Linear(feat_dim, num_classes)
+            self.lambda_max = self.mae_config.get("lambda_max", 0.1)
+            self.lambda_warmup = self.mae_config.get("lambda_warmup_steps", 1000)
+
         ########### VAE for latent space training ###########
         self.use_latent = config.get("use_latent", False)
         if self.use_latent:
@@ -51,15 +62,29 @@ class MAEPretrainModule(L.LightningModule):
     def training_step(self, batch, batch_idx):
         if isinstance(batch, (list, tuple)):
             x = batch[0]
+            labels = batch[1] if len(batch) > 1 else None
         else:
             x = batch
+            labels = None
 
         if self.use_latent:
             # x (B, 8, H, W)
             mean, _ = torch.chunk(x, 2, dim=1)
             x = (mean - self.vae_manager.mean.to(self.device)) / self.vae_manager.std.to(self.device)
 
-        loss, pred, mask = self.mae(x)
+        loss_recon, pred, mask = self.mae(x)
+
+        if self.finetune_classifier and labels is not None:
+            feats = self.encoder(x)
+            logits = self.cls_head(feats)
+            loss_ce = F.cross_entropy(logits, labels)
+            lam = min(self.lambda_max, self.lambda_max * (self.global_step + 1) / max(1, self.lambda_warmup))
+            loss = lam * loss_ce + (1 - lam) * loss_recon
+            self.log("mae_cls_loss", loss_ce, prog_bar=True)
+            self.log("mae_lambda", lam, prog_bar=False)
+        else:
+            loss = loss_recon
+
         self.log("mae_loss", loss, prog_bar=True)
         return loss
 
